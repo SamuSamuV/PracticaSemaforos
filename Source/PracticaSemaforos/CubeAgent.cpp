d2a575b89cda2bda29c39cd817147ff4e901a441
DESKTOP-GV16N45\Alumno <informatica@esne.es>
2025-04-30 16:56:55 +0200

    Codigo del CubeAgent creado

--- Source/PracticaSemaforos/CubeAgent.cpp
+++ Source/PracticaSemaforos/CubeAgent.cpp
@@ -2,20 +2,41 @@
 
 
 #include "CubeAgent.h"
+#include "Components/BoxComponent.h"
+#include "Components/InterpToMovementComponent.h"
 
 // Sets default values
 ACubeAgent::ACubeAgent()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// Set this actor to call Tick() every frame. You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	BoxCollider = CreateDefaultSubobject<UBoxComponent>(TEXT("Box Collider"));
+	RootComponent = BoxCollider;
+
+	PlatformMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Platform Mesh"));
+	PlatformMesh->SetupAttachment(RootComponent);
+
+	MovementComponent = CreateDefaultSubobject<UInterpToMovementComponent>(TEXT("Movement Component"));
+	// Setting up default parameters
+	MovementComponent->Duration = 5.0f;
+	MovementComponent->bSweep = true;
+	MovementComponent->BehaviourType = EInterpToBehaviourType::PingPong;
 }
 
 // Called when the game starts or when spawned
 void ACubeAgent::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	// Setting up the control points for movement along our path
+	MovementComponent->ControlPoints.Add(FInterpControlPoint(FVector(0.f, 0.f, 0.f), true));
+	for (int32 i = 0; i < ThePath.Num(); i++)
+	{
+		MovementComponent->ControlPoints.Add(FInterpControlPoint(ThePath[i], true));
+	}
+
+	MovementComponent->FinaliseControlPoints();
 }
 
 // Called every frame
--- Source/PracticaSemaforos/CubeAgent.h
+++ Source/PracticaSemaforos/CubeAgent.h
@@ -19,8 +19,14 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+public:
+    // Called every frame
+    virtual void Tick(float DeltaTime) override;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Pathing", meta = (ExposeOnSpawn = "true", MakeEditWidget = "true"))
+    TArray<FVector> ThePath;
 
+private:
+    UPROPERTY(EditDefaultsOnly, Category = "Components") class UBoxComponent* BoxCollider;
+    UPROPERTY(EditDefaultsOnly, Category = "Components") class UStaticMeshComponent* PlatformMesh;
+    UPROPERTY(EditDefaultsOnly, Category = "Components") class UInterpToMovementComponent* MovementComponent;
 };
